2c417af07d029d4318e1eb9b2691373392091204
MattyJ007 <matthew.dickie@jembi.org>
2021-06-18 16:39:25 +0200

    Change template component example
    The template component is now a server that will output the config data
    it received.
    This is used to demonstrate how config could be added to a component
    that would override the default config of an application
    
    DIO-28

--- docker/importer/volume/template-file-config-component/index.js
+++ docker/importer/volume/template-file-config-component/index.js
@@ -1,8 +1,27 @@
-const fs = require("fs");
+const fs = require('fs')
+const http = require('http')
+const path = require('path')
 
-const readFile = fs.readFile;
+let config = {
+  message: 'Default server config',
+  port: 7070
+}
 
-readFile('./home/node/app/config.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    console.log(data);
-});
+config = Object.assign(
+  config,
+  JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config.json')))
+)
+
+http
+  .createServer((request, response) => {
+    request
+      .on('data', chunk => {
+        console.log(`Chunk data! ${chunk}`)
+      })
+      .on('end', () => {
+        response.end(Buffer.from(config.message))
+      })
+  })
+  .listen(config.port, () => {
+    console.log(`Server running at: http://localhost:${config.port}`)
+  })
