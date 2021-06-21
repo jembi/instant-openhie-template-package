b07939513b908bd4fe10adeabf99f14867f1413f
MattyJ007 <matthew.dickie@jembi.org>
2021-06-21 11:40:31 +0200

    Simulate configuring a pre-existing component in the init script
    This template is used to show users how one could go about configuring
    Instant OpenHIE components
    
    DIO-28

--- docker/compose.sh
+++ docker/compose.sh
@@ -3,12 +3,27 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
+    # Simulating a pre-existing instant component. In this example the component is running with some default config settings.
+    # This helper container is needed to transfer files into a shared volume. A volume can only have data added if it is connected to a container.
     docker create --name file-import-helper -v template-file-config-component-volume:/app busybox
+    # Copy in demo server file
     docker cp "$composeFilePath"/importer/volume/template-file-config-component/index.js file-import-helper:/app
+    # Start up template file config container
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
+
+    # Sleep for 15 seconds to give user time to check logs for default config start up. `docker logs -f template-component`
+    sleep 15
+
+    # Add new config file to "existing" component
     docker cp "$composeFilePath"/importer/volume/template-file-config-component/config.json file-import-helper:/app
+    # Remove helper container - this container is only used to attach the shared volume for file transfer
     docker rm file-import-helper
 
-    docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/importer/docker-compose.config.yml up -d
+    # Restart exisitng component to use new config
+    docker restart template-component
+
+    # Continue with API config import
+    docker-compose -p instant -f "$composeFilePath"/importer/docker-compose.config.yml up -d
 elif [ "$1" == "up" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 elif [ "$1" == "down" ]; then
